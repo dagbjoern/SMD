054be76e818d9fb2815ce6134608d9f64c30f3a9
Dag-Björn Hering <dag.hering@udo.edu>
2016-11-02 21:37:32 +0100

    if main=name funktioniert nicht

--- Blatt 1/aufgabe2.py
+++ Blatt 1/aufgabe2.py
@@ -8,26 +8,22 @@ from scipy.optimize import curve_fit
 
 
 #Aufgabe 2
-def aufgabe2():
+
 
 #b
 
 #Definition der Funktion
-    def fx(x):
-        f=(np.sqrt(9-x)-3)/x
-        return f
+def fx(x):
+    f=(np.sqrt(9-x)-3)/x
+    return f
 
-        x=np.logspace(-1,-20,20)#Anlegen des Werte Bereiches
+x=np.logspace(-1,-20,20)#Anlegen des Werte Bereiches
 
-        plt.figure(1)
-        plt.plot(x,fx(x),'.r',label=r'$f(x)=\frac{\sqrt{9-x}-3}{x}$')
-        plt.legend(loc='best')
-        plt.grid(True)
-        plt.xscale('log')
-        plt.ylim(-0.25,0.05)
+plt.figure(1)
+plt.plot(x,fx(x),'.r',label=r'$f(x)=\frac{\sqrt{9-x}-3}{x}$')
+plt.legend(loc='best')
+plt.grid(True)
+plt.xscale('log')
+plt.ylim(-0.25,0.05)
         #plt.xlim(0.9985,1.0015)
-        plt.savefig('plot 2b).pdf')
-
-
-if __name__=='__main__':
-    aufgabe2()
+plt.savefig('plot 2b).pdf')
