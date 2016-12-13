b954b0e6ce303bb15c863b1d9ae0c9294341c542
dagbjoern <dag.hering@udo.edu>
2016-12-13 23:51:47 +0100

    Aufgabe 3 fertig Blatt6

--- Blatt6/aufgabe4.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import uncertainties.unumpy as unp
-from uncertainties.unumpy import (nominal_values as noms,
-                                  std_devs as stds)
-import scipy.constants as con
